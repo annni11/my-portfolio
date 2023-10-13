fd40aacd673f542bccca8cdc5cdee58adf891bc9
annni11 <ann.j.ni@gmail.com>
2023-10-12 21:50:03 -0400

    analytics trying again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Providers from './Providers';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang='en' className='!scroll-smooth'>
       <body className={`${inter.className} container p-2 mx-auto w-screen`}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
